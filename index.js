23ba9c5f3b7a7373502bdcb5ab95179629d6c873
Dalton Knuckles <djck244@gmail.com>
2023-03-09 13:23:45 -0700

    complete code-along

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name, value) {
+    let messageNames = []
+    for (let i = 0; i < name.length; i++) {
+        messageNames.push(`Thank you, ${name[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames;
+}
+function countDown(num) {
+    num > 0;
+    let i = num;
+    while (i > -1) {
+        console.log(num--);
+        i--;
+}
+    return num;
+}
